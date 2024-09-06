f72e25677e1d835c6059fdb275a4504d1196a74b
Miriam-Vargas-1110 <164534211+Miriam-Vargas-1110@users.noreply.github.com>
2024-09-06 12:17:46 -0600

    Add files via upload
    Ejemplos de conjuntos, tuplas y diccionarios

---
+++ Conjuntos.py
@@ -0,0 +1,5 @@
+Misgatos = {"Armin", "Luna", "Kira","khao","loki","kirita"}
+print(Misgatos)
+print("--Conjuntos loop--")
+for x in Misgatos:
+    print(x)
---
+++ Diccionario.py
@@ -0,0 +1,10 @@
+Misgatos= {
+    "ID": "1",
+    "Nombre": "Armin",
+    "edad": 8,
+    "Cumpleaños": "30 de abril"
+}
+print(Misgatos["Nombre"])
+print ("--Diccionarios loop--")
+for x in Misgatos:
+    print(x)
